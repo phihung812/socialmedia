5495f72de195402f11ee8f458e45c7f72758a217
Bùi Phi Hùng <hungbui@example.com>
2025-05-07 11:52:42 +0700

    first commit

---
+++ Client/.eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  env: { browser: true, es2020: true },
+  extends: [
+    'plugin:vue/vue3-essential',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
+  },
+};
---
+++ Client/src/views/Home.vue
@@ -0,0 +1,9 @@
+<!-- src/views/Home.vue -->
+<template>
+  <div>
+    <h1>Home Page</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
